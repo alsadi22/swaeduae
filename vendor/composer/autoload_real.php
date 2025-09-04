8e1fc4243aa7c7bdee47473a4c2c55b6d3dba6c2
SwaedUAE Website <admin@swaeduae.ae>
2025-09-04 04:43:20 +0400

    sec: enable honeypot middleware (Spatie) in web group

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit5f95ec8d21814b8e24ad5d5765c40f73
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit5f95ec8d21814b8e24ad5d5765c40f73', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit5f95ec8d21814b8e24ad5d5765c40f73', 'loadClassLoader'));
